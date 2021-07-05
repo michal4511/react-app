f269adc299e37892436ca2fc6a44bd746f9164dd
Michal <fuliermichal96@gmail.com>
2021-07-05 15:09:33 +0100

    connection between the input and the  dataResult box

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./SearchBar.css"
 
 function SearchBar({placeholder,data}) {
+
+    const [filteredData, setFilteredData] = useState([]);
+
+    const handleFilter = (event) => {
+        const searchWorld = event.target.value;
+        const newFilter = data.filter((value) => {
+            return value.full_name.includes(searchWorld);
+        });
+        setFilteredData(newFilter);
+    }
+
     return (
         <div className="search">
             <div className="inputSearch">
-                <input type="text" placeholder={placeholder}/>
+                <input type="text" placeholder={placeholder} onChange={handleFilter}/>
             </div>
-            <div className="dataResult">
-                {data.map((value, key) => {
-                    return (
-                        <a className="dataItem" href="#" key={key} target="_blank" rel="noreferrer">
-                            <p>{value.full_name}</p>
-                        </a>
-                    );
-                })}
-            </div>
+            { 
+            filteredData.length !== 0 && (
+                <div className="dataResult">
+                    {filteredData.map((value, key) => {
+                        return (
+                            <a className="dataItem" href="#" key={key} target="_blank" rel="noreferrer">
+                                <p>{value.full_name}</p>
+                            </a>
+                        );
+                    })}
+                </div>
+            )
+            }
         </div>
     )
 }
