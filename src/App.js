9e092d1179ffae3b5a1ea79f70668ffdf5e074ef
Michal <fuliermichal96@gmail.com>
2021-07-05 14:23:42 +0100

    dataResult block with customer's name is ready

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import SearchBar from './Components/SearchBar';
+import CustomerData from './MOCK_DATA.json';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Searching databases</h1>
-        <SearchBar placeholder="Enter a Customer Name..." />
+        <SearchBar placeholder="Enter a Customer Name..." data={CustomerData}/>
       </header>
     </div>
   );
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import "./SearchBar.css"
 
-function SearchBar({placeholder,key}) {
+function SearchBar({placeholder,data}) {
     return (
         <div className="search">
             <div className="inputSearch">
-                <input type="text" placeholder={placeholder} key={key}/>
+                <input type="text" placeholder={placeholder}/>
             </div>
-            <div className="dataResult"></div>
+            <div className="dataResult">
+                {data.map((value, key) => {
+                    return (
+                        <a className="dataItem" href="#" key={key} target="_blank" rel="noreferrer">
+                            <p>{value.full_name}</p>
+                        </a>
+                    );
+                })}
+            </div>
         </div>
     )
 }
