a07dd33d124c9aa4dbe7d5e806a460be0f235d8a
Michal <fuliermichal96@gmail.com>
2021-07-06 17:16:41 +0100

    added the icon with reset function

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,22 +1,44 @@
 import React, {useState} from 'react';
-import "./SearchBar.css"
+import "./SearchBar.css";
+import SearchIcon from '@material-ui/icons/Search';
+import CloseIcon from '@material-ui/icons/Close';
+import { FilterDrama } from '@material-ui/icons';
 
 function SearchBar({placeholder,data}) {
 
     const [filteredData, setFilteredData] = useState([]);
-
+    const [wordEntered, setWordEntered] = useState("");
+    
     const handleFilter = (event) => {
         const searchWorld = event.target.value;
+        setWordEntered(searchWorld);
         const newFilter = data.filter((value) => {
             return value.full_name.toLowerCase().includes(searchWorld.toLowerCase());
         });
-        setFilteredData(newFilter);
+
+        //empty input doesn't show daraResuls div
+        {searchWorld === "" ? setFilteredData([]) : setFilteredData(newFilter)};
+
+        //alternative:
+        // if (searchWorld === "") {
+        //     setFilteredData([])
+        // } else {
+        //     setFilteredData(newFilter);
+        // }
     }
+
+    const clearInput = () => {
+        setFilteredData([]);
+        setWordEntered("");
+    } 
 
     return (
         <div className="search">
-            <div className="inputSearch">
-                <input type="text" placeholder={placeholder} onChange={handleFilter}/>
+            <div className="searchInputs">
+                <input type="text" placeholder={placeholder} value={wordEntered} onChange={handleFilter}/>
+                <div className="searchIcon">
+                    {filteredData.length === 0 ? <SearchIcon/> : <CloseIcon id="clearBtn" onClick={clearInput}/>}
+                </div>
             </div>
             { 
             filteredData.length !== 0 && (
