caddfe34668d6513c1287b0380e97e7eed0f72a6
Michal <fuliermichal96@gmail.com>
2021-07-07 13:30:17 +0100

    adedd furtherInfo block with further information about customers

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,13 +1,25 @@
 import React, {useState} from 'react';
+
+// style
 import "./SearchBar.css";
+
+// icons
 import SearchIcon from '@material-ui/icons/Search';
 import CloseIcon from '@material-ui/icons/Close';
+import PersonIcon from '@material-ui/icons/Person';
+import HomeIcon from '@material-ui/icons/Home';
+import EmailIcon from '@material-ui/icons/Email';
+import PhoneIcon from '@material-ui/icons/Phone';
 
 function SearchBar({placeholder,data}) {
 
     const [filteredData, setFilteredData] = useState([]);
     const [wordEntered, setWordEntered] = useState("");
-    
+    const [userName, setUserName] = useState("");
+    const [userAddress, setUserAddress] = useState("");
+    const [userEmail, setUserEmail] = useState("");
+    const [userPhone, setUserPhone] = useState("");
+
     const handleFilter = (event) => {
         const searchWorld = event.target.value;
         setWordEntered(searchWorld);
@@ -22,7 +34,15 @@ function SearchBar({placeholder,data}) {
     const clearInput = () => {
         setFilteredData([]);
         setWordEntered("");
+        setUserName("")
     } 
+
+    const handleClick = (value) => {
+        setUserName(value.full_name);
+        setUserAddress(value.address)
+        setUserEmail(value.email);
+        setUserPhone(value.phone)
+    }
 
     return (
         <div className="search">
@@ -37,14 +57,25 @@ function SearchBar({placeholder,data}) {
                 <div className="dataResult">
                     {filteredData.map((value, key) => {
                         return (
-                            <a className="linkDataItem" href="#" key={key} target="_blank" rel="noreferrer">
-                                <p>{value.full_name}</p>
-                            </a>
+                            <button className="linkDataItem" to="/" key={key} target="_blank" rel="noreferrer" email={value.email} onClick={(e) => handleClick(value)}>
+                                <p>{value.full_name}</p> 
+                            </button>
                         );
                     })}
                 </div>
             )
             }
+            {
+            userName.length !== 0 && (
+                <div className="furtherInfo">
+                    <p><PersonIcon/>{userName}</p>
+                    <p><HomeIcon/>{userAddress}</p>
+                    <p><EmailIcon/>{userEmail}</p>
+                    <p><PhoneIcon/>{userPhone}</p>
+                </div>
+            )
+            }
+            
         </div>
     )
 }
